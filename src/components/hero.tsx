73c06bfcfc3742f35bef78cbcc2405de10cf00ef
Gie Spaepen <gie.spaepan@zna.be>
2018-01-18 13:19:51 +0100

    Stabilized Hugo, leaving it for now.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import * as A from "antwerp-core-react-branding";
 
 const Hero = () => (
-    <A.Hero title="Antwerp Core React Branding" subtitle="Overview of the styles" color="#ddbbbb">
+    <A.Hero title="Antwerp Core React Branding" subtitle="Overview of the styles" color="#f1f1f1">
     </A.Hero>
 );
 
